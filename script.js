1b53cb7a1800197fa3f3b7788d56d6e01f5899ae
KirianaBrown <kjb0369@arastudent.ac.nz>
2019-04-01 09:52:41 +1300

    Set game variables and add round score to current player value in the dom

--- script.js
+++ script.js
@@ -4,12 +4,35 @@ Click roll dice and generate a random number and set to dice value
 
 */
 
+let globalScore, roundScore, currentPlayer;
+
+currentPlayer = 0;
+roundScore = 0;
+globalScore = 0;
+
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    // 1. Generate Random Number
     let randomNumber = Math.floor(Math.random() * 6 + 1);
     console.log(randomNumber)
 
+    // 2. Update Dice Src
+
     let domDice = document.querySelector('.dice');
     domDice.src = './src/assets/dice-' + randomNumber + '.png ';
+
+    // 3. Add value to round score
+
+    if (randomNumber !== 1) {
+        roundScore += randomNumber;
+        // get inner html / text content update
+        document.querySelector('#current-' + currentPlayer).textContent = roundScore;
+    }
+
+
+
+    // Set the score to sum
+
 
 })
