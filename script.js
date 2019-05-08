dd3f48aa4aab7d7c76c6f98c4c01fb25f841351e
KirianaBrown <kjb0369@arastudent.ac.nz>
2019-05-08 16:56:33 +1200

    Game logic updated
    Includes: game playing state, init function and the final button hold and button new functions

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 /*
-
-Click roll dice and generate a random number and set to dice value
-1. On btn roll - draw random number and replace with dice image. Along with this add the score to the current score value box for the active player. 
-2. 
-
+    1. on button roll display a random dice number
 */
 
-let globalScore, roundScore, currentPlayer, scores;
+// ########################################################
+// ********* Define the variables outside of any functions so they can be accessed by all (globally) *********
+
+let globalScore, roundScore, currentPlayer, scores, gamePlaying;
 
 // ########################################################
-
 // ********* INIT FUNCTION *********
 
 init();
@@ -26,53 +24,65 @@ function init() {
     roundScore = 0;
     globalScore = 0;
     scores = [0, 0];
+    // hide dice
+    document.querySelector('.dice').style.display = 'none';
+    // remove classes
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    // reset names
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';
+    // Set game is playing to true
+    gamePlaying = true;
 }
-
 // ########################################################
 // ********* ROLL BUTTON FUNCTION *********
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    // 1. Generate Random Number
-    let randomNumber = Math.floor(Math.random() * 6 + 1);
-    console.log(randomNumber)
+    if (gamePlaying) {
+        // 1. Generate Random Number
+        let randomNumber = Math.floor(Math.random() * 6 + 1);
+        console.log(randomNumber)
 
-    // 2. Update Dice Src and display
-    let domDice = document.querySelector('.dice');
-    domDice.src = './src/assets/dice-' + randomNumber + '.png ';
-    domDice.style.display = "inline";
+        // 2. Update Dice Src and display
+        let domDice = document.querySelector('.dice');
+        domDice.src = './src/assets/dice-' + randomNumber + '.png ';
+        domDice.style.display = "inline";
 
-    // 3. Add value to round score
-    if (randomNumber !== 1) {
-        // Add round score
-        roundScore += randomNumber;
-        // get inner html / text content update
-        document.querySelector('#current-' + currentPlayer).textContent = roundScore;
-    } else {
-        console.log('You rolled a 1')
-        nextPlayer();
+        // 3. Add value to round score
+        if (randomNumber !== 1) {
+            // Add round score
+            roundScore += randomNumber;
+            // get inner html / text content update
+            document.querySelector('#current-' + currentPlayer).textContent = roundScore;
+        } else {
+            console.log('You rolled a 1')
+            roundScore = 0;
+            nextPlayer();
+        }
     }
-
 })
 
 // ########################################################
 // ********* NEXT PLAYER FUNCTION *********
 
 function nextPlayer() {
-    // Ternary Function 
+    // 1. Change current player using ternary 
     currentPlayer === 0 ? currentPlayer = 1 : currentPlayer = 0;
-    roundScore = 0;
     console.log('player has been changed')
     console.log('This should change the active state of the player')
 
-    // Reset round scores to 0
+    // 2. Reset round scores to 0
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
 
-    // Toggle the active class list on and off
+    // 3. Toggle the active class list on and off
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    // Hide original dice
+    // 4. Hide original dice
     document.querySelector('.dice').style.display = 'none';
 }
 
@@ -80,13 +90,37 @@ function nextPlayer() {
 // ********* HOLD FUNCTION *********
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-    console.log('hold button');
+    if (gamePlaying) {
+        console.log('hold button');
+        // add the round score the the current player score
+        scores[currentPlayer] += roundScore;
 
-    scores[currentPlayer] += roundScore;
+        // update the dom inner html
+        document.querySelector('#score-' + currentPlayer).textContent = scores[currentPlayer];
 
-    // add round score to the global score;
-    let foo = document.getElementById('score-' + currentPlayer).textContent = scores[currentPlayer];
+        // check if score is 100 or greater and declare winner
+        if (scores[currentPlayer] >= 10) {
+            console.log('winner')
+            document.querySelector('#name-' + currentPlayer).textContent = 'winner!';
+            // Hide dice
+            document.querySelector('.dice').style.display = 'none';
+            // add winners class
+            document.querySelector('.player-' + currentPlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + currentPlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
 
-    nextPlayer();
-
+        } else {
+            console.log('next player')
+            roundScore = 0;
+            nextPlayer();
+        }
+    }
 });
+
+// ########################################################
+// ********* NEW BUTTON FUNCTION *********
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+// ########################################################
+// ********* INIT FUNCTION *********
